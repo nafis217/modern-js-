437bcc77542b7d39813f45cddb810c5d1f8b6998
MD NAFIS AL SAFAYET <nafisalsafayet23@gmail.com>
2025-08-24 09:39:22 +0600

    Add files via upload

---
+++ arrayFindMethod.js
@@ -0,0 +1,29 @@
+// const numbers = [1, 2, 3, 4, 5, 6];
+
+// // Find the first number greater than 3
+// const result = numbers.find((num) => num > 3);
+
+// console.log(result); // Output: 4
+
+// array splice 
+
+// var numbers = [1, 2, 3, 4, 5, 6];
+
+// let result = numbers.splice(1, 2, 12, 13, 14);
+
+// console.log(numbers); // Output: [1, 12, 13, 14, 5, 6]
+
+// array concat method 
+
+// var numbers1 = [1, 2, 3, 4, 5, 6];
+// var numbers2 = [8, 9, 10];
+
+// var result = numbers1.concat(numbers2);
+
+// console.log(result); // Output: [1, 2, 3, 4, 5, 6, 8, 9, 10]
+
+// array push method 
+
+// numbers = [1, 2, 3, 4, 5, 6];
+// numbers.push(7);
+// console.log(numbers); // Output: [1, 2, 3, 4, 5, 6, 7]
